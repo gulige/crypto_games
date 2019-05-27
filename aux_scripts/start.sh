e79dc0226a444fc42513aed326c7ab7975136ba9
guli <guli@tsinghua.org.cn>
2019-05-27 21:09:54 +0800

    start sh

--- aux_scripts/start.sh
+++ aux_scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-./safe_make_no_nif.sh
 cd ../ebin
 echo "starting server......"
 erl -hidden +P 1024000 +K true -smp -name gate10000@127.0.0.1 -setcookie crypto_games_1 -boot start_sasl -config 10000 -s cg gatesvr_start -extra 127.0.0.1 52919 10000

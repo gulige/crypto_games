866b19377c3f6247492c7f9e773315eca3f1c2c3
guli <guli@tsinghua.org.cn>
2019-05-27 21:18:36 +0800

    Revert "fix"
    This reverts commit 96a958d9ac9312c083268a90eaad6bdacce99a12.

--- src/lib/chain_eos.erl
+++ src/lib/chain_eos.erl
@@ -296,7 +296,7 @@ call_contract(Contract, Action, Args, Executor) ->
             case string:tokens(Res, " ") of
                 ["executed", "transaction:", TxId | _] ->
                     list_to_binary(TxId);
-                [_, Code | _] when Code =:= "3120002:"; Code =:= "3120003:"; Code =:= "3120004:"; Code =:= "3120006:"; Code =:= "3090003:" ->
+                [_, Code | _] when Code =:= "3120002:"; Code =:= "3120003:"; Code =:= "3120004:"; Code =:= "3090003:" ->
                     make_sure_usable(),
                     call_contract(Contract, Action, Args, Executor);
                 [_, Code | _] when Code =:= "3040005:" -> % expired transaction
